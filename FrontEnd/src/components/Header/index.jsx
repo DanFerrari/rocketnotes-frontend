2c629be6f0fd8f4c9b34cce17f19c736c7c1b083
DanFerrari <danieldasilvaferrari@gmail.com>
2023-07-17 22:30:05 -0300

    fsaf

--- FrontEnd/src/components/Header/index.jsx
+++ FrontEnd/src/components/Header/index.jsx
@@ -3,11 +3,19 @@ import { useAuth } from "../../hooks/auth";
 import {RiShutDownLine} from 'react-icons/ri';
 import { api } from "../../services/api"; 
 import avatarPlaceholder from "../../assets/avatar_placeholder.svg";
-
+import { useNavigate } from "react-router-dom";
 
 export function Header(){
 
 const { signOut, user } = useAuth();
+const navigation = useNavigate();
+
+
+function handleSignOut(){
+navigation("/");
+signOut();
+}
+
 
 const avatarUrl = user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}` : avatarPlaceholder;
 
@@ -30,7 +38,7 @@ return(
 
 </Profile>
 
-<Logout onClick={signOut}>
+<Logout onClick={handleSignOut}>
   <RiShutDownLine/>
 </Logout>
 
